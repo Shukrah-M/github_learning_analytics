ce7ea6343e1c5bbd7e59a323c2ca56e858d3c40b
Shukurah <shukurahadebara01@gmail.com>
2026-07-05 23:17:43 +0100

    Add issue thread extraction

--- database/repository.py
+++ database/repository.py
@@ -1,4 +1,4 @@
-from database.models import Repository, Commit
+from database.models import Repository, Commit, Issue, IssueComment
 
 
 def save_repository(session, repository_data):
@@ -63,3 +63,78 @@ def save_commits(session, repository_id, commits_data):
     session.commit()
 
     return saved_commits
+def save_issues(session, repository_id, issues_data):
+    saved_issues = []
+
+    for issue_data in issues_data:
+        comments_url = issue_data.pop("comments_url", None)
+
+        existing_issue = (
+            session.query(Issue)
+            .filter_by(
+                repository_id=repository_id,
+                issue_number=issue_data["issue_number"]
+            )
+            .first()
+        )
+
+        if existing_issue:
+            existing_issue.github_issue_id = issue_data["github_issue_id"]
+            existing_issue.author_login = issue_data["author_login"]
+            existing_issue.title = issue_data["title"]
+            existing_issue.body = issue_data["body"]
+            existing_issue.state = issue_data["state"]
+            existing_issue.created_at = issue_data["created_at"]
+            existing_issue.updated_at = issue_data["updated_at"]
+            existing_issue.closed_at = issue_data["closed_at"]
+
+            saved_issues.append((existing_issue, comments_url))
+
+        else:
+            new_issue = Issue(
+                repository_id=repository_id,
+                **issue_data
+            )
+
+            session.add(new_issue)
+            session.flush()
+
+            saved_issues.append((new_issue, comments_url))
+
+    session.commit()
+
+    return saved_issues
+
+
+def save_issue_comments(session, issue_id, comments_data):
+    saved_comments = []
+
+    for comment_data in comments_data:
+        existing_comment = (
+            session.query(IssueComment)
+            .filter_by(
+                github_comment_id=comment_data["github_comment_id"]
+            )
+            .first()
+        )
+
+        if existing_comment:
+            existing_comment.author_login = comment_data["author_login"]
+            existing_comment.body = comment_data["body"]
+            existing_comment.created_at = comment_data["created_at"]
+            existing_comment.updated_at = comment_data["updated_at"]
+
+            saved_comments.append(existing_comment)
+
+        else:
+            new_comment = IssueComment(
+                issue_id=issue_id,
+                **comment_data
+            )
+
+            session.add(new_comment)
+            saved_comments.append(new_comment)
+
+    session.commit()
+
+    return saved_comments
